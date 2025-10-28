81d347c8db1c3427a6428ba6254b49c0530d13bc
choyDev <leodone125@gmail.com>
2025-10-28 21:28:14 +0700

    UI update

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -40,15 +40,6 @@ const navItems = [
     icon: <FaWallet className="w-5 h-5" />,
     name: "My Wallet",
     path: "/wallet",
-    subItems: [
-      { name: "Tron", path: "/wallet/tron", },
-      { name: "Solana", path: "/wallet/solana"},
-      { name: "Ethereum", path: "/wallet/ethereum", },
-      { name: "Bitcoin", path: "/wallet/bitcoin"},
-      { name: "Monero", path: "/wallet/monero", },
-      { name: "XRP", path: "/wallet/xrp"},
-      { name: "DogeCoin", path: "/wallet/dogecoin", },
-    ],
   },
   {
     icon: <FaExchangeAlt className="w-5 h-5" />,
